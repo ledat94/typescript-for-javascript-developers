6bdd231ace3464e34232764820e230a6541f51f2
ledat94 <leducdat1994@gmail.com>
2020-09-23 16:45:58 +0900

    fix ts-node-dev issue by ts-node version compatibility

--- src/boolean.ts
+++ src/boolean.ts
@@ -1,8 +1,12 @@
+//export をつけることでモジュール化できる.sử dụng modules có sẵn, ví dụ
+//như tên biến name bên dưới chả hạn. lệnh export{} có thể nằm bất cứ vị
+//trí nào trong thân file
 export {};
 
 let name = 'TypeScript';
 
 let isFinished: boolean = true;
 isFinished = false;
-// isFinished = 1;
+//isFinished = 1; ⇒Boolean chỉ cho phép true hoặc false!
+isFinished = 1;
 console.log({ isFinished });
